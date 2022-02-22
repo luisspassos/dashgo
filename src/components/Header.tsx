52e5a79219cb2b9240960cb5e2c34bbe9dba6bc5
luis <luis.passos013@gmail.com>
2022-02-21 22:00:59 -0300

    dashboard page style complete and added users listing page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
-import { Flex, Text } from '@chakra-ui/react'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
   return (
@@ -27,8 +28,72 @@ export function Header() {
         flex="1"
         py="4"
         px="8"
+        ml="6"
+        maxWidth={400}
+        alignSelf="center"
+        color="gray.200"
+        position="relative"
+        bg="gray.800"
+        borderRadius="full"
       >
+        <Input 
+          color="gray.50"
+          variant="unstyled"
+          px="4"
+          mr="4"
+          placeholder='Buscar na plataforma'
+          _placeholder={{
+            color: "gray.400"
+          }}
+        />
 
+        <Icon
+          as={RiSearchLine}
+          fontSize="20"
+        />
+      </Flex>
+
+      <Flex
+        align="center"
+        ml="auto"
+      >
+        <HStack 
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20"/>
+          <Icon as={RiUserAddLine} fontSize="20"/>
+        </HStack>
+
+        <Flex
+          align="center"
+        >
+          <Box
+            mr="4"
+            textAlign="right"
+          >
+            <Text>
+              Luís Passos
+            </Text>
+            <Text 
+              color="gray.300"
+              fontSize="small"
+            >
+              luis.passos013@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar 
+            size="md" 
+            name="Luís Passos"
+            src="https://github.com/luisspassos.png"
+          />
+        </Flex>
       </Flex>
     </Flex>
   )
